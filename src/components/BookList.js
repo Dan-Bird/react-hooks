f6ad5101152eb474397395997d227d71ebc4b09a
Daniel <info@daniel-bird.co.uk>
2020-10-29 22:05:15 +0000

    Map through books in state

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,9 +1,17 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { ThemeContext } from '../contexts/ThemeContext';
+
+const initialBookList = [
+  { title: 'the way of kings', id: 1 },
+  { title: 'the name of the wind', id: 2 },
+  { title: 'the final empire', id: 3 },
+];
 
 const BookList = () => {
   const { isLightTheme, light, dark } = useContext(ThemeContext);
   const theme = isLightTheme ? light : dark;
+
+  const [books] = useState(initialBookList);
 
   return (
     <div
@@ -11,9 +19,11 @@ const BookList = () => {
       style={{ backgroundColor: theme.bg, color: theme.syntax }}
     >
       <ul>
-        <li style={{ backgroundColor: theme.ui }}>the way of kings</li>
-        <li style={{ backgroundColor: theme.ui }}>the name of the wind</li>
-        <li style={{ backgroundColor: theme.ui }}>the final empire</li>
+        {books.map(book => (
+          <li key={book.id} style={{ backgroundColor: theme.ui }}>
+            {book.title}
+          </li>
+        ))}
       </ul>
     </div>
   );
