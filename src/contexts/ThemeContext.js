3bdc1c1d6e2092a723de50da665ff82dfc5c95ec
Daniel <info@daniel-bird.co.uk>
2020-10-29 20:52:59 +0000

    First Commit

---
+++ src/contexts/ThemeContext.js
@@ -0,0 +1,21 @@
+import React, { createContext, useState } from 'react';
+
+const themeObj = {
+  isLightTheme: true,
+  light: { syntax: '#555', ui: '#ddd', bg: '#eee' },
+  dark: { syntax: '#ddd', ui: '#333', bg: '#555' },
+};
+
+const ThemeContext = createContext(themeObj);
+
+const ThemeContextProvider = ({ children }) => {
+  const [theme] = useState(themeObj);
+
+  return (
+    <ThemeContext.Provider value={{ ...theme }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
+
+export default ThemeContextProvider;
