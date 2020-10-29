2a31e5171abeb0b198dc2bfaa45eaedb1c2f3fdb
Daniel <info@daniel-bird.co.uk>
2020-10-29 21:39:44 +0000

    Use multiple contexts

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,20 @@
 import React, { useContext } from 'react';
+import { AuthContext } from '../contexts/AuthContext';
 import { ThemeContext } from '../contexts/ThemeContext';
 
 const Navbar = () => {
+  const { isAuthenticated, toggleAuth } = useContext(AuthContext);
+
   const themeContext = useContext(ThemeContext);
   const { isLightTheme, light, dark } = themeContext;
-
   const theme = isLightTheme ? light : dark;
 
   return (
     <nav style={{ backgroundColor: theme.ui, color: theme.syntax }}>
       <h1>Context App</h1>
+      <span onClick={toggleAuth} style={{ cursor: 'pointer' }}>
+        {isAuthenticated ? 'Logged in' : 'Logged out'}
+      </span>
       <ul>
         <li>Home</li>
         <li>About</li>
