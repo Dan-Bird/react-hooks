d037d6cdc1c680fcc2a401c077c9dd5c43445f73
Daniel <info@daniel-bird.co.uk>
2020-10-30 20:43:57 +0000

    Remove books

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,31 +1,20 @@
-import React, { useContext, useState } from 'react';
-import { ThemeContext } from '../contexts/ThemeContext';
-
-const initialBookList = [
-  { title: 'the way of kings', id: 1 },
-  { title: 'the name of the wind', id: 2 },
-  { title: 'the final empire', id: 3 },
-];
+import React, { useContext } from 'react';
+import { BookContext } from '../contexts/BookContext';
+import BookDetails from './BookDetails.js';
 
 const BookList = () => {
-  const { isLightTheme, light, dark } = useContext(ThemeContext);
-  const theme = isLightTheme ? light : dark;
+  const { books } = useContext(BookContext);
 
-  const [books] = useState(initialBookList);
-
-  return (
-    <div
-      className="book-list"
-      style={{ backgroundColor: theme.bg, color: theme.syntax }}
-    >
+  return books.length ? (
+    <div className="book-list">
       <ul>
-        {books.map(book => (
-          <li key={book.id} style={{ backgroundColor: theme.ui }}>
-            {book.title}
-          </li>
-        ))}
+        {books.map(book => {
+          return <BookDetails book={book} key={book.id} />;
+        })}
       </ul>
     </div>
+  ) : (
+    <div className="empty">No books to read!</div>
   );
 };
 
